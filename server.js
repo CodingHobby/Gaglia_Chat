bab77862d381659686b25961b463056d5530a762
codinghobby@gmail.com <codinghobby@gmail.com>
2017-02-13 18:52:34 +0100

    Fixed PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.port || 3000,
+  port = process.env.PORT || 3000,
   path = require('path'),
   server = app.listen(port, function () {
     console.log('Server running on port ' + port)
