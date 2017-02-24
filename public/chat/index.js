d23b5f4f8164250ff78caa18af5b4ce585f4edb7
Corrado <corrado@codinghobby.station>
2017-02-24 19:46:00 +0000

    Added firebase integration

--- public/chat/index.js
+++ public/chat/index.js
@@ -1,48 +1,75 @@
-var msgs = []
 var notif = new Audio('./notif.mp3')
+
+var config = {
+	apiKey: "AIzaSyDZ4a2nblDl4CGL6XJBIBGdSJ55K7Ei8xs",
+	authDomain: "gagliachat.firebaseapp.com",
+	databaseURL: "https://gagliachat.firebaseio.com",
+	storageBucket: "gagliachat.appspot.com",
+	messagingSenderId: "311284835365"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database()
+
+var msgs = database.ref('msgs')
+
+
 window.addEventListener('load', function () {
-  var socket = io.connect(document.domain + ':' + location.port)
-  socket.on('msg', function (msg) {
-    msgs.push('<div class="msg"><span style="color: red"> ' + msg.user + '</span>: ' + msg.msg + '</div>')
-    updateMsgs(msgs)
-    notif.play()
+	var socket = io.connect(document.domain + ':' + location.port)
 
-    Push.create('New Message!', {
-      body: msg.user + ': ' + msg.msg,
-      icon: 'notif.png',
-      timeout: 4000,
-      onClick: function () {
-        window.focus();
-        this.close();
-      }
-    })
-  })
+	msgs.on('value', updateMsgs, function (err) {
+		console.log('Error:', err)
+	})
+	socket.on('connection', function () {
+		updateMsgs(msgs)
+	})
+
+	socket.on('msg', function (msg) {
+		msgs.push('<div class="msg"><span style="color: red"> ' + msg.user + '</span>: ' + msg.msg + '</div>')
+		updateMsgs(msgs)
+		notif.play()
+
+		Push.create('New Message!', {
+			body: msg.user + ': ' + msg.msg,
+			icon: 'notif.png',
+			timeout: 4000,
+			onClick: function () {
+				window.focus();
+				this.close();
+			}
+		})
+	})
 
 
-  document.getElementById('send').addEventListener('click', function () {
-    var userName = document.getElementById('yourId').value
-    var msg = document.getElementById('message').value
-    if (!userName || !msg) {
-      alert('Please Enter both a User Name and a Message')
-      return false
-    } else {
+	function updateMsgs(data) {
+		var keys = Object.keys(data.val())
+		document.getElementById('msgs').innerHTML = ''
+		for (var i = 0; i < keys.length; i++) {
+			console.log(data.val()[keys[i]])
+			document.getElementById('msgs').innerHTML += data.val()[keys[i]]
+		}
+	}
 
-      var msg = {
-        user: userName,
-        msg: msg
-      }
-      
-      msgs.push('<div class="msg"><span style="color: blue"> ' + msg.user + '</span>: ' + msg.msg + '</div>')
-      updateMsgs(msgs)
-      socket.emit('msg', msg)
-      document.getElementById('message').value = ''
-    }
-  })
+	document.getElementById('form').addEventListener('submit', function (e) {
+		e.preventDefault()
+		var userName = document.getElementById('yourId').value
+		var msg = document.getElementById('message').value
+		if (!userName || !msg) {
+			alert('Please Enter both a User Name and a Message')
+			return false
+		} else {
 
-  function updateMsgs(msgs) {
-    document.getElementById('msgs').innerHTML = ''
-    for (var i = 0; i < msgs.length; i++) {
-      document.getElementById('msgs').innerHTML += msgs[i]
-    }
-  }
+			var msg = {
+				user: userName,
+				msg: msg
+			}
+
+			msgs.push('<div class="msg"><span style="color: blue"> ' + msg.user + '</span>: ' + msg.msg + '</div>')
+			updateMsgs(msgs)
+			socket.emit('msg', msg)
+			document.getElementById('message').value = ''
+			return false
+		}
+	})
+
 })
